2c51f285027978ba97298d84f4d06858ed734ac4
JaemooJung <dbdbwer@naver.com>
2022-06-28 10:55:59 +0900

    o

--- sources/scene.c
+++ sources/scene.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   scene.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaemjung <jaemjung@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jaemung <jaemjung@student.42seoul.kr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 22:23:29 by jaemung           #+#    #+#             */
-/*   Updated: 2022/06/27 19:13:03 by jaemjung         ###   ########.fr       */
+/*   Updated: 2022/06/28 00:04:18 by jaemung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,22 +56,30 @@ t_scene	*scene_init(void)
 	t_scene		*scene;
 	t_object	*world;
 	t_object	*light;
-	double		ka;
 	
 	scene = (t_scene *)malloc(sizeof(t_scene));
 	if (scene == NULL)
 		error("scene malloc failed");
 	scene->canvas = canvas(WIN_W, WIN_H);
-	scene->camera = camera(&scene->canvas, point3(0, -10, 7), vec3(0, 0.6, -0.3), 120);
+	scene->camera = camera(&scene->canvas, point3(0, 0, -16), vec3(0, 0, 1), 80);
 	world = NULL;
-	obj_add(&world, object(PL, plane(point3(0, 0, -5), vec3(0, 0, 1), INFINITY), color3(155/255, 255/255, 150/255)));
-	obj_add(&world, object(SP, sphere(point3(0, 0, 3), 4), color3(0.3, 0.7, 1)));
-	obj_add(&world, object(CY, cylinder(point3(0, 0, 0), 2.2, vec3(1, 1, 1), 21.42), color3(10/255, 0, 255/255)));
-	obj_add(&world, object(CY, cylinder(point3(0, 0, 0), 30, vec3(0, 0, -1), 1.42), color3(0.7, 0.1, 0)));
+	obj_add(&world, object(PL, plane(point3(0, -1, 0), vec3(0, 1, 0), INFINITY), color3(155/255, 255/255, 150/255)));
+	obj_add(&world, object(SP, sphere(point3(0, 0, -12), 1), color3(0, 0.3, 1)));
+	obj_add(&world, object(SP, sphere(point3(0, -2, -14), 2), color3(1, 0.3, 0)));
+	obj_add(&world, object(CY, cylinder(point3(0, 0, -18), 2.2, vec3(1, 1, -1), 10), color3(10/255, 0, 255/255)));
 	scene->world = world;
-	light = object(LIGHT_POINT, light_point(point3(0, 0, 30), color3(1, 1, 1), 0.4), color3(0, 0, 0));
+	light = object(LIGHT_POINT, light_point(point3(0, 5, -10), color3(1, 1, 1), 0.4), color3(0, 0, 0));
 	scene->light = light;
-	ka = 0.2;
-	scene->ambient = vmult(color3(1,1,1), ka);
+	scene->ambient = vmult(color3(1,1,1), 0.2);
+	// scene->camera = camera(&scene->canvas, point3(0, -10, 7), vec3(0, 0.6, -0.3), 90);
+	// world = NULL;
+	// obj_add(&world, object(PL, plane(point3(0, 0, -5), vec3(0, 0, 1), INFINITY), color3(155/255, 255/255, 150/255)));
+	// obj_add(&world, object(SP, sphere(point3(0, 4, 3), 4), color3(0.3, 0.7, 1)));
+	// obj_add(&world, object(CY, cylinder(point3(0, 0, 0), 2.2, vec3(1, 1, 1), 21.42), color3(10/255, 0, 255/255)));
+	// obj_add(&world, object(CY, cylinder(point3(0, 0, 0), 30, vec3(0, 0, -1), 1.42), color3(0.7, 0.1, 0)));
+	// scene->world = world;
+	// light = object(LIGHT_POINT, light_point(point3(0, 0, 30), color3(1, 1, 1), 0.4), color3(0, 0, 0));
+	// scene->light = light;
+	// scene->ambient = vmult(color3(1,1,1), 0.2);
 	return (scene);
 }
